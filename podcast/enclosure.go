5bb8755f2c367105b80470f2a956e7e8d0f65497
LGiki <qq547221432@gmail.com>
2021-07-26 00:01:39 +0800

    Update comments

--- podcast/enclosure.go
+++ podcast/enclosure.go
@@ -5,14 +5,14 @@ import (
 	"net/http"
 )
 
+// Enclosure is the enclosure item of Podcast
 type Enclosure struct {
 	URL    string `json:"url,omitempty"`
 	Length string `json:"length,omitempty"`
 	Type   string `json:"type,omitempty"`
 }
 
-// GetEnclosureFileExtensionName
-// Determine file extension name by enclosure type first
+// GetEnclosureFileExtensionName determine file extension name by enclosure type first
 // When can not determine file extension name by enclosure type,
 // will call util.GetRemoteFileExtensionName to get file extension name
 // by sending a HTTP HEAD request
