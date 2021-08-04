46bef834d3fad2dee7e2e0011a91a765a2d0a2a7
LGiki <qq547221432@gmail.com>
2021-08-04 11:06:46 +0800

    Make download task iterator support concurrency

--- download_task_iterator.go
+++ download_task_iterator.go
@@ -9,6 +9,7 @@ import (
 type DownloadTaskIterator struct {
 	CurrentIndex         int
 	PodcastDownloadTasks []*PodcastDownloadTask
+	lock                 *sync.Mutex
 }
 
 // NewDownloadTaskIterator returns *DownloadTaskIterator init from []*PodcastDownloadTask
@@ -16,21 +17,28 @@ func NewDownloadTaskIterator(podcastDownloadTasks []*PodcastDownloadTask) *Downl
 	return &DownloadTaskIterator{
 		CurrentIndex:         0,
 		PodcastDownloadTasks: podcastDownloadTasks,
+		lock:                 &sync.Mutex{},
 	}
 }
 
 // Reset resets the current index to 0
 func (dti *DownloadTaskIterator) Reset() {
+	dti.lock.Lock()
 	dti.CurrentIndex = 0
+	dti.lock.Unlock()
 }
 
 // GetLeftLength returns left length of iterator
 func (dti *DownloadTaskIterator) GetLeftLength() int {
+	dti.lock.Lock()
+	defer dti.lock.Unlock()
 	return len(dti.PodcastDownloadTasks) - dti.CurrentIndex
 }
 
 // Next returns the next item of DownloadTaskIterator, and returns nil if next item does not exist
 func (dti *DownloadTaskIterator) Next() *PodcastDownloadTask {
+	dti.lock.Lock()
+	defer dti.lock.Unlock()
 	if dti.CurrentIndex < len(dti.PodcastDownloadTasks) {
 		podcastDownloadTask := dti.PodcastDownloadTasks[dti.CurrentIndex]
 		dti.CurrentIndex += 1
